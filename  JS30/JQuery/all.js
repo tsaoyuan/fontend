c2ab7bb2e459a2f322f46f0befa444e703810081
tsao <tsaoyuan@gmail.com>
2022-06-02 14:23:46 +0800

    新增： Animate.css 動態效果
    - CDN Animate.css
    - class name: animate__animated + 效果的 class name

---  JS30/JQuery/all.js
+++  JS30/JQuery/all.js
@@ -7,8 +7,13 @@ $(function () {
   $('.color').on('click', function () {
     $('.box').toggleClass('bg-red');
   })
+
+  $('.animate').on('click', function () {
+    $('.box').addClass('animate__animated animate__swing');
+  })
+  // 監聽 animate 作動完
+  $('.box').on('animationend',function () {
+    console.log('搖完了');
+  })
 })
 
-$(function () {
-  
-})
