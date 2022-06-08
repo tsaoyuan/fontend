68fcfa04605cbc59f2cfd26b5ec2cda6c0868a86
tsao <tsaoyuan@gmail.com>
2022-06-08 11:36:19 +0800

    feat: function parameters != argument can work
    - parameters: a, b
    - 兩個 argument, 正常輸出
    - 不足兩個 argument 或 多餘兩個 argument, 都不會出錯
    - JS 自由度高, 這個例子中顯示 JS 沒有警示功能, 警示我漏參數或是多給參數的資訊

---  JS30/Namaste_JavaScript/function.js
+++  JS30/Namaste_JavaScript/function.js
@@ -10,8 +10,16 @@
  }
 hello();
 
-hi();
-let hi = function hello() {
-  console.log('hello word!');
+// Cannot access 'hi' before initialization
+// hi();
+// let hi = function hello() {
+//   console.log('hello word!');
+// }
+
+function sayHello(a, b) {
+  console.log(a, b);
 }
+sayHello(1, 'a');
+sayHello(1);
+sayHello(1, 'a', 'a');
 
