fbfe88ce5cb7e4a9cc6bb07b9962fb1fd7bb51d1
tsao <tsaoyuan@gmail.com>
2022-06-09 16:34:20 +0800

    feat: Function 作為 return
    - return hello // 回傳沒有執行的 Function
    - 執行 Function 的方法, Function ()
    - console.log(hi()) => [Function: hello]
    - console.log(hi()()) => hello

---  JS30/5XJS/First_Class_Citizen.js
+++  JS30/5XJS/First_Class_Citizen.js
@@ -9,5 +9,13 @@ function dog() {
 }
 
 cat(dog); // I am a dog!
-cat(dog()); // TypeError: a is not a function
+// cat(dog()); // TypeError: a is not a function
 
+// return function 
+function hi() {
+  function hello() {
+    console.log('hello');
+  }
+  return hello // 回傳沒有執行的 Function
+}
+console.log(hi()());
