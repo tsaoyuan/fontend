55283558bc9fc7d4a2d698b13fbd0c011f909dab
tsao <tsaoyuan@gmail.com>
2022-06-14 17:10:57 +0800

    feat: generateComputerChoice()
    -  製造玩家click button 後, 電腦輸出的結果
    - Math.random() : a number from 0 to <1
    - Math.floor() : 3.7 -> 3; 6.45 -> 6; -5.5 -> 6, 回傳小於等於所給數字的最大整數
    - Math.floor(Math.random()*3) 輸出結果介於 0 ~ 2 之間

--- game/rock_paper_scissors/app.js
+++ game/rock_paper_scissors/app.js
@@ -5,7 +5,13 @@ const possibleChoice = document.querySelectorAll('button')
 let yourChioce
 
 possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
-  console.log(e);
   yourChioce = e.target.id
   yourChoiceDidplay.innerHTML =  yourChioce
+  generateComputerChoice()
+
 }));
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random()*3)  //  or you can use possibleChoice.length
+  console.log(randomNumber);
+}
