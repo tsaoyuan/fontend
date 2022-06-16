371a48f2bf141d434708b5b19ff5d34b420e55c2
tsao <tsaoyuan@gmail.com>
2022-06-16 18:57:36 +0800

    feat: some()
    - check 條件, return true or false
    - 無法改變原始 Array 內容
    - Array 內容 **只要有一個 element 符合 條件**, returns true

---  JS30/5XJS/research/Array_methods.js
+++  JS30/5XJS/research/Array_methods.js
@@ -5,18 +5,19 @@ const items = [
   {name: 'Book',      price: 5},
   {name: 'Phone',     price: 500},
   {name: 'Computer',  price: 1000},
-  {name: 'Keyboard',  price: 25},
-  {name: 'Book',      price: 455}
+  {name: 'Keyboard',  price: 25}
 
 ]
 
- items.forEach((item)=>{
-  console.log(item.name);
+const haveExpensive = items.some((item)=>{
+  return item.price >= 1000
 })
 
-let a = []
-items.forEach((item)=>{
-  a.push(item.name)
+ const haveFree = items.some((item)=>{
+  return item.price <= 0
 })
 
-console.log(a);
+
+console.log(haveExpensive);   // true; 只要有一個 element 符合 條件
+console.log(haveFree);        // false; 全部的 element 都沒有符合條件
+
