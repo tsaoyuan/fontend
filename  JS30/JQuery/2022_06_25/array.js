560918de003f064b0e3a03ad9d7d21865539dfb8
tsao <tsaoyuan@gmail.com>
2022-06-24 00:25:00 +0800

    feat: .filter() 找出陣列內的偶數

---  JS30/JQuery/2022_06_25/array.js
+++  JS30/JQuery/2022_06_25/array.js
@@ -1,4 +1,10 @@
 const colors = ['red', 'blue', 'green'];
 colors.forEach(function (element, index, array) {
   console.log(`第 ${index} 個元素是 ${element}`);
-})
+});
+
+const num = [1,2,3,4,5,10,9,8,7,6];
+const even = num.filter( function (element) {
+  return element % 2 == 0;
+} );
+console.log(even);
