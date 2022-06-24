fac255a1368dd2c46000c1fbc66b124525ae00a6
tsao <tsaoyuan@gmail.com>
2022-06-24 23:35:35 +0800

    feat: 選擇運算子 ||
    - 第一個數值轉換為 true, 則回傳第一個數值，否則回傳第二個

---  JS30/JQuery/2022_06_25/Operator_precedence.js
+++  JS30/JQuery/2022_06_25/Operator_precedence.js
@@ -1,2 +1,5 @@
 console.log((true || false) & false );
 console.log(true || false & false);
+console.log('\n選擇運算子');
+console.log(`true || false: ${true || false}`);
+console.log(`false || false: ${false || false}`);
